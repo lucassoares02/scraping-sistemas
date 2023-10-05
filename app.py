9221ac6f1e1d66751264248075047ffeb88d0f14
Lucas Soares <lucas.soares02@hotmail.com>
2023-10-05 20:29:43 -0300

    adicionado readme.md

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from canguru import pegar_dados_canguru
 from extrabom import pegar_dados_extrabom
+import requests
+import json
 
+response = requests.post("https://api-loja.cangurumais.com.br/v1/auth/loja/login", {
+    "domain": "cangurumais.com.br",
+    "username": "loja",
+    "key": "df072f85df9bf7dd71b6811c34bdbaa4f219d98775b56cff9dfa5f8ca1bf8469"
+})
+
+response = json.loads(response.text)
+token = f"Bearer {response['data']}"
+
+print(token)
 
 for i in range(100):
-   pegar_dados_canguru(i)
+   pegar_dados_canguru(i, token)
    pegar_dados_extrabom(i)
--- canguru.py
+++ canguru.py
@@ -3,12 +3,12 @@ import json
 from bd import inserir_dados
 
 
-def pegar_dados_canguru(i):
+def pegar_dados_canguru(i, token):
 
     url = f"https://api-loja.cangurumais.com.br/v1/loja/produtos/{i}/filial/2/centro_distribuicao/1/detalhes"
 
     # Enviar uma solicitação GET para obter o conteúdo da página
-    response = requests.get(url, headers={'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJ2aXBjb21tZXJjZSIsImF1ZCI6ImFwaS1hZG1pbiIsInN1YiI6IjZiYzQ4NjdlLWRjYTktMTFlOS04NzQyLTAyMGQ3OTM1OWNhMCIsInZpcGNvbW1lcmNlQ2xpZW50ZUlkIjpudWxsLCJpYXQiOjE2OTY1NDUxNzMsInZlciI6MSwiY2xpZW50IjpudWxsLCJvcGVyYXRvciI6bnVsbCwib3JnIjoiNDMifQ.-FnatdyjfroNGQBJHojBlL5MzU8ozB5uobAW_xIdy5nKziQTkXK18a68Qa0wgOYbhEpPoen7FvXP-j97zVB9rw'})
+    response = requests.get(url, headers={'Authorization': token})
 
     try:
         produto = json.loads(response.text)
